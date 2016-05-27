5b46bbe335f36098d1d22ee5a7ca8fd83ccd8e2e
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-05-27 13:41:35 +0300

    Fix empty postbody when split between packets
    Port of @patrickjahns work done in NONOS ver of Sming.
    Originally it was two commits:
    
    * buffer for parseHeader and parsePostData to handle split data
    * fix empty postbody when split between packets

--- sming/sming/network/HttpServerConnection.cpp
+++ sming/sming/network/HttpServerConnection.cpp
@@ -58,15 +58,10 @@ err_t HttpServerConnection::onReceive(pbuf *buf)
 
 			String contType = request.getContentType();
 			contType.toLowerCase();
-			if (request.getContentLength() > 0 && contType.indexOf(ContentType::FormUrlEncoded) != -1)
+			if (request.getContentLength() > 0 && request.getRequestMethod() == RequestMethod::POST)
 				state = eHCS_ParsePostData;
 			else
-			{
-				request.parseRawData(server, buf);
 				state = eHCS_ParsingCompleted;
-				TcpConnection::onReceive(buf);
-				return ERR_OK;
-			}				
 		}
 	}
 	else if (state == eHCS_WebSocketFrames)
